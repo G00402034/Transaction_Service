1f3d44b13765f95badae26bb45d8d40326157bea
G00402034 <G00402034@atu.ie>
2024-01-09 14:43:40 +0000

    added deposit and withdraw post mapping utilizing the Account service methods

--- src/main/java/ie/atu/transaction_service/Transaction_Controller.java
+++ src/main/java/ie/atu/transaction_service/Transaction_Controller.java
@@ -1,4 +1,35 @@
 package ie.atu.transaction_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 public class Transaction_Controller {
+
+    @RestController
+    public class Transaction_Service {
+
+        private final Transaction_Service transactionService;
+
+        @Autowired
+        public TransactionController(Transaction_Service transactionService) {
+            this.transactionService = transactionService;
+        }
+
+        @PostMapping("/deposit")
+        public ResponseEntity<String> deposit(@RequestParam long accountId, @RequestParam long amount) {
+            transactionService.deposit(accountId, amount);
+            return ResponseEntity.ok("Deposit completed successfully");
+        }
+
+        @PostMapping("/withdraw")
+        public ResponseEntity<String> withdraw(@RequestParam long accountId, @RequestParam long amount) {
+            transactionService.withdraw(accountId, amount);
+            return ResponseEntity.ok("Withdrawal completed successfully");
+        }
+    }
+
+
 }
--- src/main/java/ie/atu/transaction_service/Transaction_Service.java
+++ src/main/java/ie/atu/transaction_service/Transaction_Service.java
@@ -1,4 +1,36 @@
 package ie.atu.transaction_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class Transaction_Service {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public TransactionService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void deposit(long accountId, long amount) {
+        long currentBalance = accountService.getBalance(accountId);
+        long newBalance = currentBalance.add(amount);
+        accountService.updateBalance(accountId, newBalance);
+    }
+
+    public void withdraw(long accountId, long amount) {
+        long currentBalance = accountService.getBalance(accountId);
+        if (currentBalance.compareTo(amount) >= 0) {
+            long newBalance = currentBalance.subtract(amount);
+            accountService.updateBalance(accountId, newBalance);
+        } else {
+            throw new InsufficientFundsException("Insufficient funds for withdrawal");
+        }
+    }
 }
+
+
+
